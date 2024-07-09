a0b4a6007784f6e716cea638dcc066b938d52d06
J S <67231444+jsiplin@users.noreply.github.com>
2024-07-09 09:43:55 -0400

    Create index.js
    adding java script for main page

---
+++ index.js
@@ -0,0 +1,2 @@
+var komwelcome = document.querySelector("body > div.px-4.pt-5.my-5.text-center.border-bottom > h1").innerText
+document.querySelector("body > div.px-4.pt-5.my-5.text-center.border-bottom > h1").innerText = komwelcome +" (now with JavaScript POWER!)";
