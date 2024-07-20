1fbb1fbc1de54bd7bab8aa93a9c6f343848b81d3
Justin Siplin <jsiplin@gmail.com>
2024-07-19 23:21:23 -0400

    jquery improvements

--- js/kombucha.js
+++ js/kombucha.js
@@ -1,3 +1,8 @@
 $("h1,h2,h3,li").addClass("blue");
+$("p").addClass("fancy");
+$("button").mouseover(function(){$("button").css("background","red");})
 
+$(".expand-improve").click(function(){
+    $(".expand-improve").after("<ol><li></li></ol>");
 
+})
