4d654dcb61613b480c73979a3ea4b413700471da
Justin Siplin <jsiplin@gmail.com>
2024-07-20 00:08:04 -0400

    jquery improvements

--- js/kombucha.js
+++ js/kombucha.js
@@ -1,8 +1,17 @@
 $("h1,h2,h3,li").addClass("blue");
 $("p").addClass("fancy");
-$("button").mouseover(function(){$("button").css("background","red");})
 
 $(".expand-improve").click(function(){
-    $(".expand-improve").after("<ol><li></li></ol>");
-
+    $("body > div.alert.alert-primary > ol").remove();
+    $(".expand-improve").after("<ol> \
+            <li>HTML Added</li>\
+            <li>Cascading style sheet linked</li>\
+            <li>JavaScript enabled</li>\
+            <li>Boostrap @5.3.0</li>\
+            <li>Jquery 3.7.1</li>\
+            <li>This Drop Down</li>\
+            <li><button>Collapse</button></li>\
+        </ol>");
+        $("body > div.alert.alert-primary > ol > li:nth-child(13) > button").on
+        ("click",function(){$("body > div.alert.alert-primary > ol").remove()});
 })
