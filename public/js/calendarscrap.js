c1e0e58b93b92a114b2f099414cd281278c9efe7
Justin Siplin <jsiplin@gmail.com>
2025-02-02 23:06:41 -0500

    updated kalendar and voters

--- public/js/calendarscrap.js
+++ public/js/calendarscrap.js
@@ -6,8 +6,52 @@ function printbatches() {
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     let allevents = [
+          {
+            title: 'Brewing Mango Lime',
+            start: '2025-01-28',
+            end: '2025-02-07',
+            color: 'orange',
+            textColor: 'black'
+          },
+        
+          {
+            title: 'Tap Mango Lime',
+            start: '2025-02-07',
+            color: 'green',
+            textColor: 'white'
+          },
+        
+          {
+            title: 'Distribute Mango Lime',
+            start: '2025-02-08',
+            end: '2025-02-15',
+            color: 'yellowgreen',
+            textColor: 'black'
+          },
 
+          {
+            title: 'Brewing Lemon Ginger',
+            start: '2025-01-10',
+            end: '2025-01-17',
+            color: 'yellow',
+            textColor: 'black'
+          },
+        
+          {
+            title: 'Tap Lemon Ginger',
+            start: '2025-01-17',
+            color: 'orange',
+            textColor: 'black'
+          },
 
+          {
+            title: 'Distribute Lemon Ginger',
+            start: '2025-01-18',
+            end: '2025-01-28',
+            color: 'yellowgreen',
+            textColor: 'black'
+          },
+        
           {
             title: 'Brewing Pomegranate Ginger',
             start: '2024-12-26',
@@ -27,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
           {
             title: 'Back Home',
             url: 'http://www.sipofbooch.com',
-            start: '2025-01-31'
+            start: '2025-02-28'
           },
           {title:'Brewing Mango',start:'2024-09-19',end:'2024-09-26',color:'orangered',textColor:'black'},{title:'Tap Mango',start:'2024-09-26',color:'darkred'},{title:'Distro Mango',start:'2024-09-27',end:'2024-09-30',color:'greenyellow',textColor:'black'},{title:'Brewing Lemon Ginger Mint',start:'2024-09-09',end:'2024-09-14',color:'yellow',textColor:'black'},{title:'Tap LemGinMnt',start:'2024-09-15',color:'orange',textColor:'black'},{title:'Distro Lemon Ginger Mint',start:'2024-09-16',end:'2024-09-18',color:'greenyellow',textColor:'black'},{title:'Brewing Blackbery Mint',start:'2024-09-01',end:'2024-09-05',color:'indigo'},{title:'Tap BlkMnt',start:'2024-09-05',color:'violet',textColor:'black'},{title:'Disto BlkMnt',start:'2024-09-07',end:'2024-09-09',color:'greenyellow',textColor:'black'},{title:'First Batch Lemon Blueberry Ginger (Batch 101)',start:'2024-08-15',end:'2024-08-31'},{title:'Brewing Apple (KJP)',start:'2024-09-30',end:'2024-10-10',color:'red',textColor:'white'},{title:'Tap Apple',start:'2024-10-10',color:'crimson'},{title:'Distro Apple',start:'2024-10-11',end:'2024-10-14',color:'greenyellow',textColor:'black'},{title:'Brewing Raspberry Ginger (MP) Likely Extra Week',start:'2024-10-14',end:'2024-10-25',color:'pink',textColor:'black'},{title:'Tap Raspberry Ginger',start:'2024-10-25',color:'orange',textColor:'black'},{title:'Distro Raspbery Ginger',start:'2024-10-26',end:'2024-11-03',color:'greenyellow',textColor:'black'},{title:'Brewing Pineapple Clemintine 5 Gallon',start:'2024-11-07',end:'2024-11-14',color:'Yellow',textColor:'black'},{title:'Tap Pineapple Clemintine',start:'2024-11-14',color:'Orange',textColor:'black'},{title:'Distro Pineapple Clementine',start:'2024-11-15',end:'2024-11-20',color:'greenyellow',textColor:'black'},{title:'Brewing Strawberry Basil 5 Gallon',start:'2024-11-20',end:'2024-12-05',color:'Pink',textColor:'black'},{title:'Tap Strawberry Basil',start:'2024-12-05',color:'Red',textColor:'black'},{title:'Distro Strawberry Basil',start:'2024-12-06',end:'2024-12-12',color:'greenyellow',textColor:'black'},{title:'Holiday Break',start:'2024-12-12',end:'2024-12-26',color:'black',textColor:'white'},{title:'Tap Pomegranate Ginger',start:'2025-01-04',color:'indigo',textColor:'white'}
 
@@ -35,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function() {
       ];
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      initialDate: '2025-01-01',
+      initialDate: '2025-02-01',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
